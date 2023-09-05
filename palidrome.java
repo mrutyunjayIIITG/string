f3bd8bb64fd785181f9b63a9c7bb9401dd0b30ef
mrutyunjay <rout.mrutyunjay@gmail.com>
2023-09-05 14:19:00 -0400

    string added

---
+++ palidrome.java
@@ -0,0 +1,40 @@
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.isPalindrome(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int isPalindrome(String s) {
+        int size= s.length();
+        int l=0; int r=size-1;
+        
+        
+        while(l<r)
+        {
+            if(s.charAt(l)!=s.charAt(r))
+            {
+                return 0;
+            }
+            l++; r--;
+        }
+         return 1;        
+        
+    }
+};
